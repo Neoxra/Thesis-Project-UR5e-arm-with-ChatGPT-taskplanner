45d373ec1c012a0b79b4d0ed3de3213a6d9c1933
Nicholas Bell <76778154+Neoxra@users.noreply.github.com>
2024-09-26 14:10:19 +1000

    Delete build directory

--- build/openni2_camera/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nick/arm_ws/src/openni2_camera/openni2_camera
-# Build directory: /home/nick/arm_ws/build/openni2_camera
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("openni2_camera__py")
